0e98bb7111fb028b25be35a470d06fa2531f651f
szcchang <szcchang@cn.ibm.com>
2020-11-13 00:29:49 +0800

    999

--- src/main/java/com/ibm/cn/WydxProjectApplication.java
+++ src/main/java/com/ibm/cn/WydxProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WydxProjectApplication {
 	public static void main(String[] args) {
+		System.out.println("99");
 		SpringApplication.run(WydxProjectApplication.class, args);
 	}
 
